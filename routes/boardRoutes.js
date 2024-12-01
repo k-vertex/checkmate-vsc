969f43a3f041182f694fce0922307457fe053ffe
kangkyuchang <mhs7465@naver.com>
2024-12-02 00:55:08 +0900

    게시글 불러오기

--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 const boardController = require("../controller/boardController");
 
-router.route("/").get(boardController.getArticle);  
+router.route("/").get(boardController.getAllArticle);  
+router.route("/:articleID").get(boardController.getArticle); 
+router.route("/comment/:articleID").get(boardController.getArticleComment); 
 module.exports = router;
