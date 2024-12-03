ab9b38e99f34eddb7e5ed1eb90d49904c52eb611
kangkyuchang <70442542+kangkyuchang@users.noreply.github.com>
2024-12-03 21:36:25 +0900

    게시글 sql쿼리문 수정
    댓글 갯수 카운터하는 구문 추가

--- controller/boardController.js
+++ controller/boardController.js
@@ -1,9 +1,9 @@
 const db = require("../config/dbConnector");
 
 exports.getAllArticle = (req, res) => {
-    const { lastBoardID } = req.body;
+    const { lastBoardID } = req.query;
     const addtionalQuery = lastBoardID != null ? `(SELECT DISTINCT date FROM board WHERE board_id=${lastBoardID}) AND board_id < ${lastBoardID}` : "";
-    const query = `SELECT * FROM board b LEFT OUTER JOIN (SELECT student_id, name FROM student) s USING (student_id) ${addtionalQuery} ORDER BY date DESC LIMIT 15`;
+    const query = `SELECT *, (SELECT COUNT(*) FROM board_comment WHERE board_id = b.board_id) AS cmt_cnt FROM board b LEFT OUTER JOIN (SELECT student_id, name FROM student) s USING (student_id) ${addtionalQuery} ORDER BY date DESC LIMIT 15`;
     db.query(query, (err, results) => {
         if (err) {
             console.error("게시글 가져오는 중 오류:", err);
@@ -19,15 +19,15 @@ exports.getAllArticle = (req, res) => {
 }
 
 exports.getArticle = (req, res) => {
-    const { articleID } = req.body;
-    const query = "SELECT * FROM board b LEFT OUTER JOIN (SELECT student_id, name FROM student) s USING (student_id) WHERE board_id = ?;";
+    const { articleID } = req.query;
+    const query = "SELECT *, (SELECT COUNT(*) FROM board_comment WHERE board_id = b.board_id) AS cmt_cnt FROM board b LEFT OUTER JOIN (SELECT student_id, name FROM student) s USING (student_id) WHERE board_id = ?;";
     db.query(query, [articleID], (err, results) => {
         if (err) {
             console.error("게시글 가져오는 중 오류:", err);
             res.status(500).send("오류");
         } 
         else if (results.length > 0) {
-            rse.status(200).json(results);
+            res.status(200).json(results);
         } 
         else {
             res.status(200).send("해당 게시글이 없음");
@@ -36,15 +36,15 @@ exports.getArticle = (req, res) => {
 }
 
 exports.getArticleComment = (req, res) => {
-    const { articleID } = req.body;
-    const query = "SELECT * FROM board_comment WHERE board_id=?";
+    const { articleID } = req.query;
+    const query = "SELECT * FROM board_comment c LEFT OUTER JOIN (SELECT student_id, name FROM student) s USING (student_id) WHERE board_id=?";
     db.query(query, [articleID], (err, results) => {
         if (err) {
             console.error("게시글 가져오는 중 오류:", err);
             res.status(500).send("오류");
         } 
         else if (results.length > 0) {
-            rse.status(200).json(results);
+            res.status(200).json(results);
         } 
         else {
             res.status(200).send("해당 게시글에 댓글이 없음");
